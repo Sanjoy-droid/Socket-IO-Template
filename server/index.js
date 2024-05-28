5d7e1b62c88d81cb71a550722d7a6a6058ab2380
Sanjoy-droid <sanjoy@sanjoydroid.com>
2024-05-28 19:54:42 +0530

    .env added along with sample endpoint for testing api

--- server/index.js
+++ server/index.js
@@ -5,12 +5,17 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 app.use(cors());
 
+const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173"],
     methods: ["GET", "POST"],
   },
+});
+//sample endpoint testing
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
 });
 
 io.on("connection", (socket) => {
@@ -20,6 +25,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log("listening on *:3001");
+server.listen(PORT, () => {
+  console.log(`app listening on port:http://localhost:${PORT}`);
 });
