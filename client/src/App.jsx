e6ee07317f0ae5341a014d50aa3ab46de83c327d
Sanjoy-droid <sanjoy@sanjoydroid.com>
2024-05-28 09:21:33 +0530

    Socket IO template with basic implementation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:5000");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+  const [messageRecieve, setMessageRecieve] = useState("");
+  const sendMessage = () => {
+    socket.emit("send_message", { message });
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageRecieve(data.message);
+    });
+  }, [socket]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className=" flex justify-center items-center h-48 ">
+        <div className="">
+          <input
+            className="border-2 border-black h-10"
+            onChange={(e) => setMessage(e.target.value)}
+            type="text"
+            placeholder="Type Your Message"
+          />
+          <button
+            className="ml-4 bg-blue-800 text-white w-10"
+            onClick={sendMessage}
+          >
+            Send
+          </button>
+          <h1>{messageRecieve}</h1>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
